9867089804346535aa663c6dc3e7ecc9f0c53be0
kuno_lab <kuno_lab@kuno-lab-no-MacBook-Pro.local>
2013-03-15 16:20:43 +0900

    Initial Commit

---
+++ OpenCVTest/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  OpenCVTest
+//
+//  Created by kuno_lab on 2013/03/15.
+//  Copyright (c) 2013年 kuno_lab. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
