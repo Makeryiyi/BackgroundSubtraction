2661693e803a5423d3b44cd67ce9cc1f90d003e3
kuno_lab <kuno_lab@kuno-lab-no-MacBook-Pro.local>
2013-05-24 16:40:10 +0900

    pthread meanFilter

--- meanFilter/main.cpp
+++ meanFilter/main.cpp
@@ -11,38 +11,70 @@
 #include <highgui.h>
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <pthread.h>
 #include <unistd.h>
 #include <time.h>
 
-void meanFilter(IplImage *srcImage, IplImage *dstImage, int size) {
-    int step = srcImage->width-size;
-    if (step < 0) step = srcImage->width;
-    
-    for (int rr = 0; rr < srcImage->height;rr++) {
-        for (int c=0;c<srcImage->width;c++) {
-            unsigned int r=0,g=0,b=0;
+struct Arg {
+    IplImage *srcImage;
+    IplImage *dstImage;
+    int size;
+    int num;
+};
+
+void *filter(void *_arg) {
+    Arg *arg = (Arg *)_arg;
+    IplImage *srcImage = arg->srcImage;
+    IplImage *dstImage = arg->dstImage;
+    int size = arg->size;
+    int num = arg->num;
+    printf("%d\n", num);
+    for (int r = 0; r < srcImage->height;r++) {
+        for (int c=0;c<srcImage->width;c+=4) {
+            int cc = c + num;
+            unsigned int rc=0,gc=0,bc=0;
             for (int i = 0;i<size;i++) {
                 for (int j = 0;j<size;j++) {
-                    int tc = (c+j-size/2);
-                    int tr = (rr+i-size/2);
+                    int tc = (cc+j-size/2);
+                    int tr = (r+i-size/2);
                     if (tc < 0 || tc > srcImage->width || tr<0 || tr>=srcImage->height) {
                     } else {
-                        b += (unsigned int)(unsigned char)srcImage->imageData[tc*3+0+tr*srcImage->widthStep];
-                        g += (unsigned int)(unsigned char)srcImage->imageData[tc*3+1+tr*srcImage->widthStep];
-                        r += (unsigned int)(unsigned char)srcImage->imageData[tc*3+2+tr*srcImage->widthStep];
+                        bc += (unsigned int)(unsigned char)srcImage->imageData[tc*3+0+tr*srcImage->widthStep];
+                        gc += (unsigned int)(unsigned char)srcImage->imageData[tc*3+1+tr*srcImage->widthStep];
+                        rc += (unsigned int)(unsigned char)srcImage->imageData[tc*3+2+tr*srcImage->widthStep];
                     }
                 }
             }
-            b /= size*size;
-            g /= size*size;
-            r /= size*size;
-            dstImage->imageData[c*3+0+rr*dstImage->widthStep] = (char)(unsigned char)b;
-            dstImage->imageData[c*3+1+rr*dstImage->widthStep] = (char)(unsigned char)g;
-            dstImage->imageData[c*3+2+rr*dstImage->widthStep] = (char)(unsigned char)r;
+            bc /= size*size;
+            gc /= size*size;
+            rc /= size*size;
+            dstImage->imageData[cc*3+0+r*dstImage->widthStep] = (char)(unsigned char)bc;
+            dstImage->imageData[cc*3+1+r*dstImage->widthStep] = (char)(unsigned char)gc;
+            dstImage->imageData[cc*3+2+r*dstImage->widthStep] = (char)(unsigned char)rc;
         }
     }
+    return NULL;
 }
+
+void meanFilter(IplImage *srcImage, IplImage *dstImage, int size)
+{
+    static int NUM = 4;
+    pthread_t th[NUM];
+    struct Arg arg[4] = {
+        {srcImage, dstImage, size, 0},
+        {srcImage, dstImage, size, 1},
+        {srcImage, dstImage, size, 2},
+        {srcImage, dstImage, size, 3}
+    };
+    for (int i=0;i<NUM;i++) {
+        pthread_create(&th[i], NULL, filter, &arg[i]);
+    }
+    for (int i=0;i<NUM;i++) {
+        pthread_join(th[i], NULL);
+    }
+}
+
 
 int main() {
     IplImage *srcImage = cvLoadImage("/Users/kuno_lab/Pictures/bg.jpg");
@@ -53,7 +85,7 @@ int main() {
     printf("start\n");
     start = clock();
 
-    meanFilter(srcImage, dstImage, 20);
+    meanFilter(srcImage, dstImage, 51);
 
     end = clock();
     printf("end\n");
